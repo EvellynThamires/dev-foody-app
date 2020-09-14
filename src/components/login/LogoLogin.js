f7abe1576530f29e58a62ad6fbeefda99b2d56e0
Evellyn <xe.uyemura@gmail.com>
2020-09-14 16:27:24 -0300

    adicionando a pasta plugins no gitinore

--- src/components/login/LogoLogin.js
+++ src/components/login/LogoLogin.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import logo from '../../img/Logo.png'
+import logo from '../../assets/images/logo.png'
 
 const Logo = styled.img `
     display: block;
